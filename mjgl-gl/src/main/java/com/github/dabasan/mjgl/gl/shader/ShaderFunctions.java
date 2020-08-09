d41f6e48bdc8acbc125f0a48f1264425397f4899
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-09 17:50:42 +0900

    Added gl module.

---
+++ mjgl-gl/src/main/java/com/github/dabasan/mjgl/gl/shader/ShaderFunctions.java
@@ -0,0 +1,158 @@
+package com.github.dabasan.mjgl.gl.shader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GL2ES2;
+import com.jogamp.opengl.GLContext;
+
+/**
+ * Shader functions
+ * 
+ * @author Daba
+ *
+ */
+class ShaderFunctions {
+	private static Logger logger = LoggerFactory.getLogger(ShaderFunctions.class);
+
+	public static int createProgram(InputStream isVShader, InputStream isFShader)
+			throws IOException {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int vShaderID = gl.glCreateShader(GL2ES2.GL_VERTEX_SHADER);
+		int fShaderID = gl.glCreateShader(GL2ES2.GL_FRAGMENT_SHADER);
+
+		// Load vertex shader.
+		var vShaderCode = new ArrayList<String>();
+		try (var brVShader = new BufferedReader(new InputStreamReader(isVShader, "UTF-8"))) {
+			while (true) {
+				String line = brVShader.readLine();
+				if (line == null) {
+					break;
+				}
+
+				vShaderCode.add(line);
+			}
+		}
+
+		// Load fragment shader.
+		var fShaderCode = new ArrayList<String>();
+		try (var brFShader = new BufferedReader(new InputStreamReader(isFShader, "UTF-8"))) {
+			while (true) {
+				String line = brFShader.readLine();
+				if (line == null) {
+					break;
+				}
+
+				fShaderCode.add(line);
+			}
+		}
+
+		String[] vShaderLines = new String[vShaderCode.size()];
+		vShaderCode.toArray(vShaderLines);
+
+		String[] fShaderLines = new String[fShaderCode.size()];
+		fShaderCode.toArray(fShaderLines);
+
+		// Append LF to every line.
+		for (int i = 0; i < vShaderLines.length; i++) {
+			vShaderLines[i] += "\n";
+		}
+		for (int i = 0; i < fShaderLines.length; i++) {
+			fShaderLines[i] += "\n";
+		}
+
+		IntBuffer result = Buffers.newDirectIntBuffer(1);
+
+		// Compile vertex shader.
+		gl.glShaderSource(vShaderID, vShaderLines.length, vShaderLines, null);
+		gl.glCompileShader(vShaderID);
+
+		// Check vertex shader.
+		gl.glGetShaderiv(vShaderID, GL2ES2.GL_COMPILE_STATUS, result);
+		if (result.get(0) == GL2ES2.GL_FALSE) {
+			String errMessage = getShaderInfoLog(gl, vShaderID);
+
+			logger.error("Vertex shader compilation failed.");
+			logger.error(errMessage);
+
+			return -1;
+		}
+
+		// Compile fragment shader.
+		gl.glShaderSource(fShaderID, fShaderLines.length, fShaderLines, null);
+		gl.glCompileShader(fShaderID);
+
+		// Check fragment shader.
+		gl.glGetShaderiv(fShaderID, GL2ES2.GL_COMPILE_STATUS, result);
+		if (result.get(0) == GL2ES2.GL_FALSE) {
+			String errMessage = getShaderInfoLog(gl, fShaderID);
+
+			logger.error("Fragment shader compilation failed.");
+			logger.error(errMessage);
+
+			return -1;
+		}
+
+		// Link program.
+		int programID = gl.glCreateProgram();
+		gl.glAttachShader(programID, vShaderID);
+		gl.glAttachShader(programID, fShaderID);
+		gl.glLinkProgram(programID);
+
+		// Check program.
+		gl.glGetProgramiv(programID, GL2ES2.GL_LINK_STATUS, result);
+		if (result.get(0) == GL2ES2.GL_FALSE) {
+			String errMessage = getProgramInfoLog(gl, programID);
+
+			logger.error("Program link failed.");
+			logger.error(errMessage);
+
+			return -1;
+		}
+
+		gl.glDeleteShader(vShaderID);
+		gl.glDeleteShader(fShaderID);
+
+		logger.info("Successfully created a program. programID={}", programID);
+
+		return programID;
+	}
+	private static String getShaderInfoLog(GL2ES2 gl, int shaderID) {
+		IntBuffer infoLogLength = Buffers.newDirectIntBuffer(1);
+		gl.glGetShaderiv(shaderID, GL2ES2.GL_INFO_LOG_LENGTH, infoLogLength);
+
+		ByteBuffer buffer = Buffers.newDirectByteBuffer(infoLogLength.get(0));
+		gl.glGetShaderInfoLog(shaderID, infoLogLength.get(0), null, buffer);
+		buffer.flip();
+
+		var arrBytes = new byte[buffer.remaining()];
+		buffer.get(arrBytes);
+		String message = new String(arrBytes);
+
+		return message;
+	}
+	private static String getProgramInfoLog(GL2ES2 gl, int programID) {
+		IntBuffer infoLogLength = Buffers.newDirectIntBuffer(1);
+		gl.glGetProgramiv(programID, GL2ES2.GL_INFO_LOG_LENGTH, infoLogLength);
+
+		ByteBuffer buffer = Buffers.newDirectByteBuffer(infoLogLength.get(0));
+		gl.glGetProgramInfoLog(programID, infoLogLength.get(0), null, buffer);
+		buffer.flip();
+
+		var arrBytes = new byte[buffer.remaining()];
+		buffer.get(arrBytes);
+		String message = new String(arrBytes);
+
+		return message;
+	}
+}
---
+++ mjgl-gl/src/main/java/com/github/dabasan/mjgl/gl/shader/ShaderProgram.java
@@ -0,0 +1,144 @@
+package com.github.dabasan.mjgl.gl.shader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.FloatBuffer;
+
+import com.github.dabasan.ejml_3dtools.Matrix;
+import com.github.dabasan.ejml_3dtools.Vector;
+import com.github.dabasan.mjgl.gl.color.Color;
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GL2ES2;
+import com.jogamp.opengl.GLContext;
+
+/**
+ * Shader program
+ * 
+ * @author Daba
+ *
+ */
+public class ShaderProgram {
+	private int programID;
+
+	public ShaderProgram(InputStream isVShader, InputStream isFShader) throws IOException {
+		this.constructorBase(isVShader, isFShader);
+	}
+	public ShaderProgram(File fileVShader, File fileFShader) throws IOException {
+		try (var fisVShader = new FileInputStream(fileVShader);
+				var fisFShader = new FileInputStream(fileFShader)) {
+			this.constructorBase(fisVShader, fisFShader);
+		}
+	}
+	public ShaderProgram(String filepathVShader, String filepathFShader) throws IOException {
+		try (var fisVShader = new FileInputStream(filepathVShader);
+				var fisFShader = new FileInputStream(filepathFShader)) {
+			this.constructorBase(fisVShader, fisFShader);
+		}
+	}
+	private void constructorBase(InputStream isVShader, InputStream isFShader) throws IOException {
+		programID = ShaderFunctions.createProgram(isVShader, isFShader);
+	}
+
+	public boolean isValid() {
+		return (programID < 0) ? false : true;
+	}
+
+	public int getProgramID() {
+		return programID;
+	}
+
+	public void enable() {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+		gl.glUseProgram(programID);
+	}
+
+	// int
+	public void setUniform(String name, int value) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform1i(location, value);
+	}
+	public void setUniform(String name, int value0, int value1) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform2i(location, value0, value1);
+	}
+	public void setUniform(String name, int value0, int value1, int value2) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform3i(location, value0, value1, value2);
+	}
+	public void setUniform(String name, int value0, int value1, int value2, int value3) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform4i(location, value0, value1, value2, value3);
+	}
+	// float
+	public void setUniform(String name, float value) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform1f(location, value);
+	}
+	public void setUniform(String name, float value0, float value1) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform2f(location, value0, value1);
+	}
+	public void setUniform(String name, float value0, float value1, float value2) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform3f(location, value0, value1, value2);
+	}
+	public void setUniform(String name, float value0, float value1, float value2, float value3) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform4f(location, value0, value1, value2, value3);
+	}
+	// Vector
+	public void setUniform(String name, Vector value) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform3f(location, value.getXFloat(), value.getYFloat(), value.getZFloat());
+	}
+	// Color
+	public void setUniform(String name, Color value) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniform4f(location, value.getR(), value.getG(), value.getB(), value.getA());
+	}
+	// Matrix
+	public void setUniform(String name, Matrix value, boolean transpose) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		FloatBuffer buffer = Buffers.newDirectFloatBuffer(16);
+		double[] arr = value.toArray();
+		for (int i = 0; i < 16; i++) {
+			buffer.put(i, (float) arr[i]);
+		}
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glUniformMatrix4fv(location, 1, transpose, buffer);
+	}
+
+	// Texture
+	public void setTexture(String name, int textureUnit, int textureTarget, int textureID) {
+		GL2ES2 gl = GLContext.getCurrentGL().getGL2ES2();
+
+		int location = gl.glGetUniformLocation(programID, name);
+		gl.glActiveTexture(GL2ES2.GL_TEXTURE0 + textureUnit);
+		gl.glBindTexture(textureTarget, textureID);
+		gl.glUniform1i(location, textureUnit);
+	}
+}

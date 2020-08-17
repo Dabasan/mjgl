b1f4bf56ca8db1e843bb914de9c84231ba2443f8
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-17 15:05:02 +0900

    Updated ShaderFileVisitor.

--- mjgl-gl/src/main/java/com/github/dabasan/mjgl/gl/util/ShaderFileVisitor.java
+++ mjgl-gl/src/main/java/com/github/dabasan/mjgl/gl/util/ShaderFileVisitor.java
@@ -3,6 +3,7 @@ package com.github.dabasan.mjgl.gl.util;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
@@ -32,7 +33,17 @@ class ShaderFileVisitor implements FileVisitor<Path> {
 	}
 	@Override
 	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-		dirs.add(file.getParent());
+		boolean dirAlreadyVisited = false;
+		for (var dir : dirs) {
+			if (Files.isSameFile(dir, file.getParent())) {
+				dirAlreadyVisited = true;
+				break;
+			}
+		}
+
+		if (dirAlreadyVisited == false) {
+			dirs.add(file.getParent());
+		}
 
 		return FileVisitResult.CONTINUE;
 	}

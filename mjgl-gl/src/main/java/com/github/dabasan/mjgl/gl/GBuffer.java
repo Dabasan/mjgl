3ab7b0d19e477e8e7c2731954c96e3a3face80bf
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-08-26 21:46:49 +0900

    Updated GBuffer.

--- mjgl-gl/src/main/java/com/github/dabasan/mjgl/gl/GBuffer.java
+++ mjgl-gl/src/main/java/com/github/dabasan/mjgl/gl/GBuffer.java
@@ -42,32 +42,18 @@ public class GBuffer {
 		texPosition = textures.get(1);
 		texNormal = textures.get(2);
 
-		gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, texAlbedo);
-		gl.glTexImage2D(GL3ES3.GL_TEXTURE_2D, 0, GL3ES3.GL_RGBA32F, bufferWidth, bufferHeight, 0,
-				GL3ES3.GL_RGBA, GL3ES3.GL_FLOAT, null);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MAG_FILTER, GL3ES3.GL_NEAREST);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MIN_FILTER, GL3ES3.GL_NEAREST);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_S, GL3ES3.GL_REPEAT);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_T, GL3ES3.GL_REPEAT);
-		gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, 0);
-
-		gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, texPosition);
-		gl.glTexImage2D(GL3ES3.GL_TEXTURE_2D, 0, GL3ES3.GL_RGB32F, bufferWidth, bufferHeight, 0,
-				GL3ES3.GL_RGB, GL3ES3.GL_FLOAT, null);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MAG_FILTER, GL3ES3.GL_NEAREST);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MIN_FILTER, GL3ES3.GL_NEAREST);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_S, GL3ES3.GL_REPEAT);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_T, GL3ES3.GL_REPEAT);
-		gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, 0);
-
-		gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, texNormal);
-		gl.glTexImage2D(GL3ES3.GL_TEXTURE_2D, 0, GL3ES3.GL_RGB32F, bufferWidth, bufferHeight, 0,
-				GL3ES3.GL_RGB, GL3ES3.GL_FLOAT, null);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MAG_FILTER, GL3ES3.GL_NEAREST);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MIN_FILTER, GL3ES3.GL_NEAREST);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_S, GL3ES3.GL_REPEAT);
-		gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_T, GL3ES3.GL_REPEAT);
-		gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, 0);
+		for (int i = 0; i < 3; i++) {
+			gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, textures.get(i));
+			gl.glTexImage2D(GL3ES3.GL_TEXTURE_2D, 0, GL3ES3.GL_RGB32F, bufferWidth, bufferHeight, 0,
+					GL3ES3.GL_RGB, GL3ES3.GL_FLOAT, null);
+			gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MAG_FILTER,
+					GL3ES3.GL_NEAREST);
+			gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_MIN_FILTER,
+					GL3ES3.GL_NEAREST);
+			gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_S, GL3ES3.GL_REPEAT);
+			gl.glTexParameteri(GL3ES3.GL_TEXTURE_2D, GL3ES3.GL_TEXTURE_WRAP_T, GL3ES3.GL_REPEAT);
+			gl.glBindTexture(GL3ES3.GL_TEXTURE_2D, 0);
+		}
 	}
 	private void setupFramebuffer() {
 		GL3ES3 gl = GLContext.getCurrentGL().getGL3ES3();
@@ -103,13 +89,13 @@ public class GBuffer {
 	protected int getFBO() {
 		return fbo;
 	}
-	protected int getTexAlbedo() {
+	public int getTexAlbedo() {
 		return texAlbedo;
 	}
-	protected int getTexPosition() {
+	public int getTexPosition() {
 		return texPosition;
 	}
-	protected int getTexNormal() {
+	public int getTexNormal() {
 		return texNormal;
 	}
 

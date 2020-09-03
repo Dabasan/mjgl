142838cc21f23f70b20f25b504306de857dea204
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-09-03 11:42:13 +0900

    Added UV output.

--- mjgl-gl/MJGL/Shader/Default/3D/Model/Geometry/fshader.glsl
+++ mjgl-gl/MJGL/Shader/Default/3D/Model/Geometry/fshader.glsl
@@ -1,16 +1,16 @@
 #version 330
 
-uniform sampler2D textureSampler;
-
 in vec3 vsOutPosition;
 in vec2 vsOutUV;
 in vec3 vsOutNormal;
-layout(location=0) out vec4 fsOutAlbedo;
+layout(location=0) out float fsOutAlbedo;
 layout(location=1) out vec3 fsOutPosition;
-layout(location=2) out vec3 fsOutNormal;
+layout(location=2) out vec2 fsOutUV;
+layout(location=3) out vec3 fsOutNormal;
 
 void main(){
-    fsOutAlbedo=texture(textureSampler,vsOutUV);
+    fsOutAlbedo=1.0;
     fsOutPosition=vsOutPosition;
+    fsOutUV=vsOutUV;
     fsOutNormal=vsOutNormal;
 }

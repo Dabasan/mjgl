998ec2b8d0519134167154e6e3e12470ec273835
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-09-05 22:56:43 +0900

    Changed output shapes of a fragment shader.

--- mjgl-gl/MJGL/Shader/Default/3D/Model/Geometry/fshader.glsl
+++ mjgl-gl/MJGL/Shader/Default/3D/Model/Geometry/fshader.glsl
@@ -5,12 +5,12 @@ uniform sampler2D tex;
 in vec3 vsOutPosition;
 in vec2 vsOutUV;
 in vec3 vsOutNormal;
-layout(location=0) out vec3 fsOutColor;
-layout(location=1) out vec3 fsOutPosition;
-layout(location=3) out vec3 fsOutNormal;
+layout(location=0) out vec4 fsOutColor;
+layout(location=1) out vec4 fsOutPosition;
+layout(location=2) out vec4 fsOutNormal;
 
 void main(){
-    fsOutColor=texture(tex,vsOutUV).rgb;
-    fsOutPosition=vsOutPosition;
-    fsOutNormal=vsOutNormal;
+    fsOutColor=vec4(texture(tex,vsOutUV).rgb,1.0);
+    fsOutPosition=vec4(vsOutPosition,1.0);
+    fsOutNormal=vec4(vsOutNormal,1.0);
 }
